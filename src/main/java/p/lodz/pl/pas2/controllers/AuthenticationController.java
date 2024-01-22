fc2431948019a02c73c484119c22080b2bc1de32
Kacper Kafara <kacperkafara18@gmail.com>
2024-01-22 01:00:46 +0100

    ETag at login

--- src/main/java/p/lodz/pl/pas2/controllers/AuthenticationController.java
+++ src/main/java/p/lodz/pl/pas2/controllers/AuthenticationController.java
@@ -1,6 +1,8 @@
 package p.lodz.pl.pas2.controllers;
 
+import com.nimbusds.jose.JOSEException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import p.lodz.pl.pas2.Dto.UserDto.LoginDto;
 import p.lodz.pl.pas2.request.LoginRequest;
 import p.lodz.pl.pas2.Dto.UserDto.UserDto;
+import p.lodz.pl.pas2.security.Jws;
 import p.lodz.pl.pas2.security.UserAuthProvider;
 import p.lodz.pl.pas2.services.AuthenticationService;
 
@@ -19,12 +22,15 @@ import p.lodz.pl.pas2.services.AuthenticationService;
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
     private final UserAuthProvider userAuthProvider;
+    private final Jws jws;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginDto> loginUser(@RequestBody LoginRequest user) {
+    public ResponseEntity<LoginDto> loginUser(@RequestBody LoginRequest user) throws JOSEException {
         LoginDto authUserDto = authenticationService.loginUser(user);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("ETag", jws.generateSign(authUserDto.getId()));
         authUserDto.setToken(userAuthProvider.createToken(authUserDto.getId(), authUserDto.getUserType()));
-        return ResponseEntity.status(HttpStatus.OK).body(authUserDto);
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(authUserDto);
     }
 
 }
